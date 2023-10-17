ea8ee98154406b1b97313aa9c8a4d258ed3cb081
Gabriele Corti <borntofrappe@gmail.com>
2023-10-17 18:44:50 +0200

    svg icons
    export an object to inline the SVG syntax

---
+++ src/routes/icons.js
@@ -0,0 +1,19 @@
+const icons = {
+	expand: `<svg width="1em" height="1em" viewBox="-10 -10 20 20">
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M 9.5 -9.5 3 -9.5 9 -9 9.5 -3 Z M 9.5 -9.5 -9.5 9.5 M -9.5 9.5 -3 9.5 -9 9 -9.5 3 Z" />
+</svg>`,
+	pause: `<svg width="1em" height="1em" viewBox="-10 -10 20 20">
+    <path fill="currentColor" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" d="M -6.5 -8 -4 -8 -4 8 -6.5 8 Z M 6.5 -8 4 -8 4 8 6.5 8 Z" />
+</svg>`,
+	reset: `<svg width="1em" height="1em" viewBox="-10 -10 20 20">
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M -6 -9.5 -9.5 -6 -6 -2.5 -9 -6 Z M -9.5 -6 L 2.5 -6 A 7 7 0 0 1 9.5 1 8.5 8.5 0 0 1 -7.5 1" />
+</svg>`,
+	restore: `<svg width="1em" height="1em" viewBox="-10 -10 20 20">
+    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M 9.5 -9.5 2 -2 2 -8 2.5 -2.5 8 -2 2 -2 M -9.5 9.5 -2 2 -8 2 -2.5 2.5 -2 8 -2 2" />
+</svg>`,
+	start: `<svg width="1em" height="1em" viewBox="-10 -10 20 20">
+    <path fill="currentColor" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" d="M -6.5 -8 7 0 -6.5 8 Z" />
+</svg>`
+};
+
+export default icons;
