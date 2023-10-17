0787d094943ef3d8f88a617470fd0e6fe48313a3
Gabriele Corti <borntofrappe@gmail.com>
2023-10-17 18:49:57 +0200

    add webfont
    include the subset inter-regular font for the 0-9 digits, full period, semicolon, 'hr', 'min', 'sec' characters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,22 @@
 	import icons from './icons';
 </script>
 
-<p>add icons {@html icons['expand']}</p>
+<div>
+	<h1>01:23:45.99</h1>
+
+	<ul>
+		<li>hr</li>
+		<li>min</li>
+		<li>sec</li>
+	</ul>
+</div>
+
+<style>
+	div {
+		font-family: 'Inter', system-ui, sans-serif;
+	}
+
+	h1 {
+		font-weight: 400;
+	}
+</style>
