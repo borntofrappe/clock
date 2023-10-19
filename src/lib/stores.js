86815925aff5c2f22d2128d3860a81f49e31fbcd
Gabriele Corti <borntofrappe@gmail.com>
2023-10-19 22:50:07 +0200

    refactor store to use interval
    the issue with requestAnimationFrame is that the function is paused as the browser prioritizes other tabs

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,28 +2,26 @@ import { writable } from 'svelte/store';
 
 const createStopwatch = () => {
 	let running = false;
-	let frame = 0;
+	/** @type null|NodeJS.Timer */
+	let interval = null;
 	let ms = 0;
 	let lapsed = 0;
 
 	const { subscribe, set } = writable(0, () => {
 		return () => {
-			cancelAnimationFrame(frame);
+			if (interval) clearInterval(interval);
 		};
 	});
-
-	const run = () => {
-		set(lapsed + (new Date().valueOf() - ms));
-		frame = requestAnimationFrame(run);
-	};
 
 	const toggle = () => {
 		if (running) {
 			lapsed += new Date().valueOf() - ms;
-			cancelAnimationFrame(frame);
+			if (interval) clearInterval(interval);
 		} else {
 			ms = new Date().valueOf();
-			frame = requestAnimationFrame(run);
+			interval = setInterval(() => {
+				set(lapsed + (new Date().valueOf() - ms));
+			});
 		}
 
 		running = !running;
