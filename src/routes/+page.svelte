a6d3fedcbc13d5eb420eb91ad416164fbf5545e7
Gabriele Corti <borntofrappe@gmail.com>
2023-10-19 22:31:47 +0200

    use stopwatch custom store
    pay attention that it is not possible to use the value of the store on the server as the store relies on the document and requestAnimationFrame/ cancelAnimationFrame being available

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import icons from './icons';
+	import { stopwatch } from '$lib/stores';
+	import { getTime } from '$lib/utils';
+	import { onMount } from 'svelte';
 
 	let expand = false;
 
@@ -14,6 +17,18 @@
 			expand = !expand;
 		}
 	};
+
+	let [hours, minutes, seconds, hundredths] = new Array(4).fill('00');
+
+	onMount(() => {
+		const unsubscribe = stopwatch.subscribe((ms) => {
+			[hours, minutes, seconds, hundredths] = getTime(ms).map((d) => d.toString().padStart(2, '0'));
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
 </script>
 
 <div class="center">
@@ -26,22 +41,22 @@
 		<!-- prettier-ignore -->
 		<svg aria-level={1} role="heading" viewBox="-9.04542 -11.79565 85.964 17.841">
         <g fill="currentColor" font-family="'Inter', system-ui, sans-serif">
-            <text text-anchor="middle"><tspan font-size="16">00</tspan><tspan x="0" y="6" font-size="4">hr</tspan></text>
+            <text text-anchor="middle"><tspan font-size="16">{hours}</tspan><tspan x="0" y="6" font-size="4">hr</tspan></text>
             <text x="10.049788" font-size="16">:</text>
-            <text x="24.508667" text-anchor="middle"><tspan font-size="16">00</tspan><tspan x="24.508667" y="6" font-size="4">min</tspan></text>
+            <text x="24.508667" text-anchor="middle"><tspan font-size="16">{minutes}</tspan><tspan x="24.508667" y="6" font-size="4">min</tspan></text>
             <text x="34.558453" font-size="16">:</text>
-            <text x="49.017334" text-anchor="middle"><tspan font-size="16">00</tspan><tspan x="49.017334" y="6" font-size="4">sec</tspan></text>
+            <text x="49.017334" text-anchor="middle"><tspan font-size="16">{seconds}</tspan><tspan x="49.017334" y="6" font-size="4">sec</tspan></text>
             <text x="59.06712" font-size="16">.</text>
-            <text x="70.699295" font-size="11" text-anchor="middle">00</text>
+            <text x="70.699295" font-size="11" text-anchor="middle">{hundredths}</text>
         </g>
       </svg>
 
 		<div class="controls">
-			<button>
+			<button on:click={stopwatch.toggle}>
 				<span class="visually-hidden">Start</span>
 				{@html icons['start']}
 			</button>
-			<button>
+			<button on:click={stopwatch.reset}>
 				<span class="visually-hidden">Reset</span>
 				{@html icons['reset']}
 			</button>
