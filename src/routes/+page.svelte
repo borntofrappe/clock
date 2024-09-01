6010732034af7b6d317376410587e9cfb4d7fc14
Gabriele Corti <borntofrappe@gmail.com>
2024-09-01 15:00:48 +0200

    lap feature
    complete app functionalities

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+  import { onDestroy } from "svelte";
+  import { getTimeComponents } from "$lib/utils";
+  import type { Lap, Addendum } from "$lib/types";
+
   import Icons from "./Icons.svelte";
   import Preferences from "./Preferences.svelte";
   import Display from "./Display.svelte";
-  import { getTimeComponents } from "$lib/utils";
-  import { onDestroy } from "svelte";
+  import Laps from "./Laps.svelte";
 
   type Mode = "wait" | "run" | "pause";
 
   let ms = $state(0);
   let time = $derived(getTimeComponents(ms));
   let mode: Mode = $state("wait");
+  let laps: Lap[] = $state([]);
 
   let lapsed = 0;
   let startMs = 0;
@@ -52,6 +56,53 @@
 
     stop();
     mode = "wait";
+    laps = [];
+  };
+
+  const setLap = () => {
+    if (mode !== "run") return;
+
+    const msTotal = ms;
+    let lap: Lap;
+    const { length } = laps;
+
+    if (length === 0) {
+      lap = {
+        number: length + 1,
+        msCurrent: msTotal,
+        msTotal,
+      };
+    } else {
+      const msCurrent = msTotal - laps[0].msTotal;
+
+      lap = {
+        number: length + 1,
+        msCurrent,
+        msTotal,
+      };
+
+      if (length === 1) {
+        const [currentAddendum, previousAddendum]: Addendum[] =
+          msCurrent < laps[0].msCurrent
+            ? ["Fastest", "Slowest"]
+            : ["Slowest", "Fastest"];
+        lap.addendum = currentAddendum;
+        laps[0].addendum = previousAddendum;
+      } else {
+        const slowestIndex = laps.findIndex((d) => d.addendum === "Slowest");
+        const fastestIndex = laps.findIndex((d) => d.addendum === "Fastest");
+
+        if (msCurrent < laps[fastestIndex].msCurrent) {
+          lap.addendum = "Fastest";
+          delete laps[fastestIndex].addendum;
+        } else if (msCurrent > laps[slowestIndex].msCurrent) {
+          lap.addendum = "Slowest";
+          delete laps[slowestIndex].addendum;
+        }
+      }
+    }
+
+    laps = [lap, ...laps];
   };
 </script>
 
@@ -87,6 +138,13 @@
           <use href="#icon-pause" width="1" height="1" />
         </svg>
     </button>
+    <button id="set-lap" disabled={mode !== "run"} onclick={setLap}>
+      <span class="visually-hidden">Set lap</span>
+      <!-- prettier-ignore -->
+      <svg width="1em" height="1em" viewBox="0 0 1 1">
+        <use href="#icon-flag" width="1" height="1" />
+      </svg>
+    </button>
     <button id="reset" disabled={mode === "wait"} onclick={reset}>
       <span class="visually-hidden">Reset stopwatch</span>
       <!-- prettier-ignore -->
@@ -94,5 +152,8 @@
           <use href="#icon-reset" width="1" height="1" />
         </svg>
     </button>
+  </div>
+  <div id="laps">
+    <Laps {laps} />
   </div>
 </div>
---
+++ src/routes/Laps.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { formatLapMs } from "$lib/utils";
+  import type { Lap } from "$lib/types";
+
+  let { laps }: { laps: Lap[] } = $props();
+</script>
+
+<table>
+  <thead>
+    <tr>
+      <th>Laps</th>
+      <th>Time</th>
+      <th>Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each laps as { number, addendum, msCurrent, msTotal }}
+      <tr>
+        <td>
+          {number}
+          {#if addendum}
+            <span> &nbsp;{addendum}</span>
+          {/if}
+        </td>
+        <td>
+          <time>{formatLapMs(msCurrent)}</time>
+        </td>
+        <td>
+          <time>{formatLapMs(msTotal)}</time>
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
